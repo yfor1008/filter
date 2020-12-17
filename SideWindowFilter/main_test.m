e03c23cf77a13b5d5319c692ddf8f5582d454c77
yfor <yfor1008@gmail.com>
2020-12-17 14:34:01 +0800

    [feat] add side window filter

---
+++ SideWindowFilter/main_test.m
@@ -0,0 +1,11 @@
+close all; clear; clc;
+
+im = imread('./src/lena.jpg');
+result = im;
+result1(:,:,1) = medfilt2(im(:,:,1), [11, 11]);
+result1(:,:,2) = medfilt2(im(:,:,2), [11, 11]);
+result1(:,:,3) = medfilt2(im(:,:,3), [11, 11]);
+result2 = SideWindowBoxFilter(im, 5, 1);
+
+im_show = cat(2, im, result1, uint8(result2));
+imshow(im_show)
